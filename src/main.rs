6dcac0e7a054ac90f52f1f3fe11045d3f37a5332
Olle Wreede <olle@wreede.se>
2022-07-15 17:22:16 +0200

    Center zooming

--- src/main.rs
+++ src/main.rs
@@ -57,12 +57,13 @@ uniform sampler2D Texture;
 
 void main() {
     float time = _Time.x;
-    vec2 coord = gl_FragCoord.xy/canvasSize.xy;
+    vec2 coord = gl_FragCoord.xy / canvasSize.xy; // [0,1]
+    coord = 2. * (coord - 1.); // [-1,1]
     float scale = sin(time) + 1.;
     coord = coord / scale;
-    if (coord.x > 0. && coord.x < 1. &&
-        coord.y > 0. && coord.y < 1.) {
-        gl_FragColor = vec4(coord.x, coord.y, 1.-coord.x, 1);
+    if (abs(coord.x) < 1. && abs(coord.y) < 1.) {
+        coord = (coord + 1.) / 2.;
+        gl_FragColor = vec4(coord.x, coord.y, 1. - coord.x, 1);
     } else {
         gl_FragColor = vec4(1,1,1,1); // white
     }
